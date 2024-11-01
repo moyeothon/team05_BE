67e5cb9abe97085bfeba2ad11e0ae58f136a3f07
Junyeong-An <2117ab@gmail.com>
2024-11-01 21:26:43 +0900

    [refactor] Swagger 경로 수정

--- src/main/java/projects/moyethon/global/SwaggerConfig.java
+++ src/main/java/projects/moyethon/global/SwaggerConfig.java
@@ -1,20 +1,34 @@
 package projects.moyethon.global;
 
-
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
 public class SwaggerConfig {
+
     @Bean
     public OpenAPI openAPI() {
+
+        Server testServer = new Server()
+                .url("https://junyeongan.store")
+                .description("배포 서버");
+
+        Server localServer = new Server()
+                .url("http://localhost:8080")
+                .description("로컬 테스트");
+
         return new OpenAPI()
                 .components(new Components())
-                .info(apiInfo());
+                .info(apiInfo())
+                .servers(List.of(localServer, testServer));
     }
+
     private Info apiInfo() {
         return new Info()
                 .title("Mute API")
