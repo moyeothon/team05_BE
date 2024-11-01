8f01c115e49d253de5f30433c6643eb38b75bb55
Junyeong-An <2117ab@gmail.com>
2024-11-01 23:12:11 +0900

    [refactor] 다이어리 조회 로직 수정

--- src/main/java/projects/moyethon/repository/DiaryRepository.java
+++ src/main/java/projects/moyethon/repository/DiaryRepository.java
@@ -1,10 +1,11 @@
 package projects.moyethon.repository;
 
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import projects.moyethon.domain.Diary;
 
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
 
-    Optional<Diary> findByMemberNickname(String userId);
+    List<Diary> findByMemberNickname(String userNickname);
 }
